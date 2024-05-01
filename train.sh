878043f2dbd57eaff3b407a08c796314f3fd2e24
davnish <nischal.singh38@gmail.com>
2024-05-01 18:41:09 +0530

    updated script

--- train.sh
+++ train.sh
@@ -1,3 +1,6 @@
-python main.py --epoch 1 --eval 1 --lr 0.0001 --step_size 100 --points_taken 4096 --batch_size 16 --grid_size 25 --model_name 1 --model NPCT
-python main.py --epoch 1 --eval 1 --lr 0.0001 --step_size 100 --points_taken 4096 --batch_size 16 --grid_size 25 --model_name 1 --model SPCT
-python main.py --epoch 1 --eval 1 --lr 0.0001 --step_size 100 --points_taken 4096 --batch_size 16 --grid_size 25 --model_name 1 --model PCT
+model_name = 1
+epoch = 2
+
+python main.py --epoch $epoch --eval 1 --lr 0.0001 --step_size 100 --points_taken 4096 --batch_size 8 --grid_size 25 --model_name $model_name --model NPCT 
+python main.py --epoch $epoch --eval 1 --lr 0.0001 --step_size 100 --points_taken 4096 --batch_size 8 --grid_size 25 --model_name $model_name --model SPCT
+python main.py --epoch $epoch --eval 1 --lr 0.0001 --step_size 100 --points_taken 4096 --batch_size 8 --grid_size 25 --model_name $model_name --model PCT
