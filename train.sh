713fff7f5fa642b7b4cb56ae662bec5c2c353872
davnish <nischal.singh38@gmail.com>
2024-05-02 15:09:48 +0530

    fixed typo

--- train.sh
+++ train.sh
@@ -3,9 +3,9 @@ epoch=2
 eval=1
 embd=64
 
-python main.py --epoch $epoch --eval $eval --lr 0.0001 --step_size 100 --points_taken 4096 --batch_size 8 --grid_size 25 --model_name $model_name --model NPCT 
-python main.py --epoch $epoch --eval $eval --lr 0.0001 --step_size 100 --points_taken 4096 --batch_size 8 --grid_size 25 --model_name $model_name --model SPCT
-python main.py --epoch $epoch --eval $eval --lr 0.0001 --step_size 100 --points_taken 4096 --batch_size 8 --grid_size 25 --model_name $model_name --model PCT
-python main.py --epoch $epoch --eval $eval --lr 0.0001 --step_size 100 --points_taken 4096 --batch_size 8 --grid_size 25 --model_name $model_name --model PCT_FP
-python main.py --epoch $epoch --eval $eval --lr 0.0001 --step_size 100 --points_taken 4096 --batch_size 8 --grid_size 25 --model_name $model_name --model PCT_FPMOD
-python main.py --epoch $epoch --eval $eval --lr 0.0001 --step_size 100 --points_taken 4096 --batch_size 8 --grid_size 25 --model_name $model_name --model PCT_FPADV
+python train.py --epoch $epoch --eval $eval --lr 0.0001 --step_size 100 --points_taken 4096 --batch_size 8 --grid_size 25 --model_name $model_name --model NPCT 
+python train.py --epoch $epoch --eval $eval --lr 0.0001 --step_size 100 --points_taken 4096 --batch_size 8 --grid_size 25 --model_name $model_name --model SPCT
+python train.py --epoch $epoch --eval $eval --lr 0.0001 --step_size 100 --points_taken 4096 --batch_size 8 --grid_size 25 --model_name $model_name --model PCT
+python train.py --epoch $epoch --eval $eval --lr 0.0001 --step_size 100 --points_taken 4096 --batch_size 8 --grid_size 25 --model_name $model_name --model PCT_FP
+python train.py --epoch $epoch --eval $eval --lr 0.0001 --step_size 100 --points_taken 4096 --batch_size 8 --grid_size 25 --model_name $model_name --model PCT_FPMOD
+python train.py --epoch $epoch --eval $eval --lr 0.0001 --step_size 100 --points_taken 4096 --batch_size 8 --grid_size 25 --model_name $model_name --model PCT_FPADV
